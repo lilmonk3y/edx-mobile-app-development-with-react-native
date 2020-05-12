e161c18fce4d9edf6739a9042273e2e32e090121
Christian Nahuel Rivera <christiannahuelrivera@gmail.com>
2020-05-12 00:40:27 -0300

    refactor

--- project0/script.js
+++ project0/script.js
@@ -12,51 +12,34 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 /*
 TODO: 
-  - unchecked count span has a bug while unchecking and checking items.
-  - refactor
+  - agregar lógica de delete de tarea
   - pull request
 */
 
 function newTodo() {
   // add elements to list
   var textOfItem = prompt("Enter your todo!")
+  addTodoItem(textOfItem)
+  
+  // increment item count
+  let itemsSize = Number(itemCountSpan.innerHTML)
+  let newSize = itemsSize + 1
+  itemCountSpan.innerHTML = newSize.toString()
 
+  // increment unchecked items count 
+  itemsSize = Number(uncheckedCountSpan.innerHTML)
+  newSize = itemsSize + 1
+  uncheckedCountSpan.innerHTML = newSize.toString()
+}
 
+function addTodoItem(textOfItem){
   var listItem = document.createElement(classNames.TODO_ITEM)
 
   var propertyCheckBox = document.createElement(classNames.TODO_CHECKBOX)
   var checkbox = document.createElement('INPUT')
   checkbox.setAttribute("type", "checkbox");
   checkbox.checked = false
-  checkbox.onclick = function(){
-    if(this.checked){
-      let itemsSize = Number(uncheckedCountSpan.innerHTML)
-      let newSize = itemsSize - 1
-      uncheckedCountSpan.innerHTML = newSize.toString()
-
-      let item = this.parentElement.parentElement
-      let originalText = item.childNodes[1].textContent
-      let strike = document.createElement('strike')
-      let strikeText = document.createTextNode(originalText)
-      strike.appendChild(strikeText)
-      var propertyTextOfItem = document.createElement(classNames.TODO_TEXT)
-      propertyTextOfItem.appendChild(strike)
-
-      item.replaceChild(propertyTextOfItem, item.childNodes[1])
-    }else{
-      let itemsSize = Number(uncheckedCountSpan.innerHTML)
-      let newSize = itemsSize + 1
-      uncheckedCountSpan.innerHTML = newSize.toString()
-
-      let item = this.parentElement.parentElement
-      let originalText = item.childNodes[1].childNodes[0].textContent
-      let newText = document.createTextNode(originalText)
-      var propertyTextOfItem = document.createElement(classNames.TODO_TEXT)
-      propertyTextOfItem.appendChild(newText)
-
-      item.replaceChild(propertyTextOfItem, item.childNodes[1])
-    }
-  }
+  checkbox.onclick = checkboxOnClick
   propertyCheckBox.appendChild(checkbox)
   listItem.appendChild(propertyCheckBox)
 
@@ -68,12 +51,34 @@ function newTodo() {
   var row = document.createElement('li')
   row.appendChild(listItem)
   list.appendChild(row)
-  
-  // increment item count
-  let itemsSize = Number(itemCountSpan.innerHTML)
-  let newSize = itemsSize + 1
-  itemCountSpan.innerHTML = newSize.toString()
+}
 
-  // increment unchecked items count 
-  uncheckedCountSpan.innerHTML = newSize.toString()
+function checkboxOnClick(){
+  if(this.checked){
+    let itemsSize = Number(uncheckedCountSpan.innerHTML)
+    let newSize = itemsSize - 1
+    uncheckedCountSpan.innerHTML = newSize.toString()
+
+    let item = this.parentElement.parentElement
+    let originalText = item.childNodes[1].textContent
+    let strike = document.createElement('strike')
+    let strikeText = document.createTextNode(originalText)
+    strike.appendChild(strikeText)
+    var propertyTextOfItem = document.createElement(classNames.TODO_TEXT)
+    propertyTextOfItem.appendChild(strike)
+
+    item.replaceChild(propertyTextOfItem, item.childNodes[1])
+  }else{
+    let itemsSize = Number(uncheckedCountSpan.innerHTML)
+    let newSize = itemsSize + 1
+    uncheckedCountSpan.innerHTML = newSize.toString()
+
+    let item = this.parentElement.parentElement
+    let originalText = item.childNodes[1].childNodes[0].textContent
+    let newText = document.createTextNode(originalText)
+    var propertyTextOfItem = document.createElement(classNames.TODO_TEXT)
+    propertyTextOfItem.appendChild(newText)
+
+    item.replaceChild(propertyTextOfItem, item.childNodes[1])
+  }
 }
